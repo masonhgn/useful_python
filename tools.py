7439f01cc95a1f2743d1b20858b474b78d9b0a3b
Mason <73012906+masonhgn@users.noreply.github.com>
2023-04-03 01:27:37 -0400

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,7 @@
+#these are some useful ways to do things in python that I don't want to forget about
+
+
+
+#how to convert a list of strings into a list of ints
+nums = ['123','412']
+nums = list(map(int, nums))
